84520e44d12b7d55019f3eb1412b1790ef089b78
Schleidens <alectineschleidens2016@gmail.com>
2021-04-13 18:28:29 -0400

    navbar compo done

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,26 +1,26 @@
 <template>
   <header class="sticky top-0 left-0 main-menu">
-    <div class="container flex flex-wrap p-5 flex-col md:flex-row items-center">
+    <div class="container flex flex-wrap p-4 md:flex-row items-center">
       <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
         <span class="ml-3 text-xl text-white font-bold">NuxtLover</span>
       </a>
-      <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center menu">
+      <nav class="md:ml-auto hidden md:flex flex-wrap items-center text-base justify-center menu">
         <a class="mr-5 menu--list">First Link</a>
         <a class="mr-5 menu--list">Second Link</a>
         <a class="mr-5 menu--list">Fourth Link</a>
         <a class="mr-5 menu--list">Third Link</a>
       </nav>
-      <div class="mobile-btn">
+      <button class="mobile-btn outline-none focus-none md:hidden focus:outline-none" @click="toogle">
         <div class="bar" />
         <div class="bar" />
         <div class="bar" />
-      </div>
+      </button>
     </div>
-    <div class="mobile-nav">
+    <div class="mobile-nav" :class="show ? 'visible' : ''">
       <div class="py-1" role="none">
-        <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Account settings</a>
-        <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Support</a>
-        <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">License</a>
+        <a href="#" class="block px-4 menu--list" @click="toogle">Account settings</a>
+        <a href="#" class="block px-4 menu--list" @click="toogle">Support</a>
+        <a href="#" class="block px-4 menu--list" @click="toogle">License</a>
       </div>
     </div>
   </header>
@@ -28,7 +28,17 @@
 
 <script>
 export default {
+  data () {
+    return {
+      show: false
+    }
+  },
 
+  methods: {
+    toogle () {
+      this.show = !this.show
+    }
+  }
 }
 </script>
 
@@ -38,15 +48,49 @@ export default {
 }
 
 .bar{
-  color: white;
   border: 1px solid white;
-  height: 1px;
+  background-color: white;
+
+  height: 3px;
+
   margin: 5px;
   width: 30px;
 }
 
+.bar:nth-child(1){
+  width: 15px;
+}
+
+.bar:nth-child(3){
+  width: 15px;
+  margin-left: 20px;
+}
+
+.mobile-btn{
+  right: 10px;
+  position: absolute;
+  top: 20px;
+}
+
 .mobile-nav{
   float: right;
+  position: absolute;
+
+  color:white;
+  background-color: #11DCA3;
+
+  top: 50px;
+  right: 50px;
+
+  clip-path: circle(0vmax at 50% 0%);
+  transition: clip-path .5s ease-in-out, 0s opacity 1s;
+}
+
+.visible{
+  display: flex;
+
+  clip-path: circle(140vmax at 50% 0%);
+  transition: clip-path .5s linear;
 }
 
 .menu--list:hover{
