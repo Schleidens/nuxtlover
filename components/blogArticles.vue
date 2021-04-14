0196be588d7ff0219ac137d9a7a0324a278d1a22
Schleidens <alectineschleidens2016@gmail.com>
2021-04-13 20:18:14 -0400

    design blog articles section

---
+++ components/blogArticles.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="text-gray-600 body-font">
+    <div class="container px-5 py-24 mx-auto">
+      <div class="flex flex-wrap -m-4">
+        <div class="p-4 w-full background-articles">
+          <div class="h-full flex sm:flex-row flex-col items-center sm:justify-start text-justify sm:text-left">
+            <div class="flex-shrink-0 sm:w-64 w-full object-cover object-center mb-4 sm:mb-0">
+              <img alt="team" class="rounded-lg" src="https://res.cloudinary.com/nuxt/image/upload/w_1200,h_675,c_fill,f_auto/remote/nuxt-org/blog/creating-nuxt-module/main.png">
+            </div>
+            <div class="flex-grow sm:pl-8">
+              <h2 class="title-font font-medium text-lg text-gray-400">
+                Holden Caulfield
+              </h2>
+              <p class="mb-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas labore repudiandae similique odio nobis provident voluptatum quis quam. Odio magni doloremque qui deleniti iure enim eius neque a? Neque, eveniet.
+              </p>
+              <span class="inline-flex">
+                #Nuxt #Date-nfs #tailwind
+              </span>
+              <span class="inline-flex">
+                #Nuxt #Date-nfs #tailwind
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
