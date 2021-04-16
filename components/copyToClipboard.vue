ce88cd9077fff008f56f0090666f4ba9a66b55af
Schleidens <alectineschleidens2016@gmail.com>
2021-04-16 17:10:51 -0400

    copy link to clipboard compo

---
+++ components/copyToClipboard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <button class="bg-gray-900 p-2 w-full rounded-md text-gray-400 mt-8" @click="copyLink">
+      {{ buttonText }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      buttonText: 'Copy link to clipboard',
+      success: 'Copied'
+    }
+  },
+  methods: {
+    async copyLink () {
+      await navigator.clipboard.writeText(window.location.href)
+      this.buttonText = this.success
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
