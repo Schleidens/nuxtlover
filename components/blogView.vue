c029e7ce4ebf506027583e3c079834d0f30076ef
Schleidens <alectineschleidens2016@gmail.com>
2021-04-15 21:05:23 -0400

    slot compo bog pages

---
+++ components/blogView.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="px-2 sm:px-56 py-8 sm:py-16">
+    <div>
+      <slot name="img" />
+    </div>
+
+    <slot />
+
+    <div>
+      <slot name="author" />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
