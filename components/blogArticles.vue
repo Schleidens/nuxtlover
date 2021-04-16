8d1c3ab753ff594e171e69e25d6ae255428a16ca
Schleidens <alectineschleidens2016@gmail.com>
2021-04-15 21:06:54 -0400

    clean code and modis design

--- components/blogArticles.vue
+++ components/blogArticles.vue
@@ -1,13 +1,13 @@
 <template>
-  <section class="text-gray-600 body-font">
-    <div class="container px-5 py-10 mx-auto">
+  <section class="text-gray-600 body-font rounded bg-gray-900 mx-4 sm:mx-8 my-8 py-6 px-6">
+    <div class="container mx-auto">
       <div class="flex flex-wrap -m-4">
-        <div class="p-4 w-full background-articles">
+        <div class="w-full background-articles">
           <div class="h-full flex sm:flex-row flex-col items-center sm:justify-start text-justify sm:text-left">
             <div class="flex-shrink-0 sm:w-64 w-full object-cover object-center mb-4 sm:mb-0">
               <slot name="img" />
             </div>
-            <div class="flex-grow sm:pl-8">
+            <div class="flex-grow sm:pl-2">
               <h2 class="title-font font-medium text-lg text-gray-400">
                 <slot name="title" />
               </h2>
