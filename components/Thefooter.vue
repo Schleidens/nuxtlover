aba213753f4e668864649e7a181d42caea81c7d8
Schleidens <alectineschleidens2016@gmail.com>
2021-04-15 21:04:02 -0400

    footer added to all pages

---
+++ components/Thefooter.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="bg-gray-800">
+      <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+        <p class="text-gray-500 text-sm text-center sm:text-left">
+          build with &#128147; by Schleidens.dev
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
