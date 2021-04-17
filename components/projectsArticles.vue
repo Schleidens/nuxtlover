7435e68d2460f9acb817617e63dccadc1b037023
Schleidens <alectineschleidens2016@gmail.com>
2021-04-17 17:32:35 -0400

    projects page and slug page done

---
+++ components/projectsArticles.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="border border-gray-700 text-gray-600 body-font rounded bg-gray-900 mx-4 sm:mx-8 my-8 py-6 px-6">
+    <div class="container mx-auto">
+      <div class="flex flex-wrap -m-4">
+        <div class="p-2 w-full">
+          <h2 class="text-lg text-gray-200 text-2xl font-bold">
+            <slot name="title" />
+          </h2>
+          <p class="leading-relaxed text-base">
+            <slot />
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/projectsView.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="px-2 md:px-56 py-8 md:py-16">
+    <div class="bg-gray-900 p-6 rounded-md text-4xl font-bold mb-4 text-gray-100">
+      <slot name="title" />
+    </div>
+    <div class="bg-gray-900 p-6 rounded-md mb-4 text-gray-100">
+      <slot />
+    </div>
+    <div class="bg-gray-900 p-6 rounded-md mb-4 text-gray-100">
+      <slot name="author" />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
