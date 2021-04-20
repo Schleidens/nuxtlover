9edfe98b205fe6e2ffdfb4d7f0e9bca5e1805625
Schleidens <alectineschleidens2016@gmail.com>
2021-04-20 19:06:41 -0400

    add nuxt svg to footer

--- components/Thefooter.vue
+++ components/Thefooter.vue
@@ -1,10 +1,20 @@
 <template>
   <div>
     <div class="bg-gray-800">
+      <div class="container mx-auto py-4 px-5">
+        <p class="text-gray-500 text-sm text-center uppercase">
+          <a href="" class="ml-6">Twitter</a>
+          <a href="" class="ml-6">Github</a>
+          <a href="" class="ml-6">Discord</a>
+        </p>
+      </div>
       <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
         <p class="text-gray-500 text-sm text-center sm:text-left">
           build with &#128147; by Schleidens.dev
         </p>
+        <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+          <img src="../assets/svg/built-with-nuxt-white.svg" alt="NuxtJs" class="w-36 h-6">
+        </span>
       </div>
     </div>
   </div>
